cfe7fed9c3a504b5f4ceeffc5de205abba939ffb
Taylor Smith <contact@tdhsmith.com>
2016-06-27 14:23:11 -0500

    Add language file

---
+++ resources/lang/en/validation.php
@@ -0,0 +1,13 @@
+<?php 
+
+/*
+ * These are the default messages for the new rule types.
+ *
+ * You can override these by placing your own translation file at:
+ * resources/lang/vendor/RuleExtensions/en/validation.php
+ */
+
+return [
+    'carbon_date' => 'The :attribute field could not be parsed by Eloquent as a (Carbon) datetime.',
+    
+];
